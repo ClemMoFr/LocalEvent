e6b7032df33cf829fd5d272b5c15c260032017fb
Clément <clmentmoreno@hotmail.com>
2023-09-01 18:42:07 +0200

    navbar ok

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import RoutesPaths from "./config/RoutesPaths";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  const [stateNavbar, setStateNavbar] = useState("home");
   return (
     <BrowserRouter>
       <RoutesPaths />
+      <Navbar
+        stateNavbar={stateNavbar}
+        handleNavHome={() => setStateNavbar("home")}
+        handleNavFavorite={() => setStateNavbar("favorite")}
+        handleNavSearch={() => setStateNavbar("search")}
+        handleNavPanel={() => setStateNavbar("panel")}
+        handleNavSetting={() => setStateNavbar("settings")}
+      />
     </BrowserRouter>
   );
 }
