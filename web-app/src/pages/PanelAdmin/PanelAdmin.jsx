e1887d1ce76011867b7fbbcde0940c308a8096d6
Clément <clmentmoreno@hotmail.com>
2023-09-02 18:38:03 +0200

    main part is done

--- web-app/src/pages/PanelAdmin/PanelAdmin.jsx
+++ web-app/src/pages/PanelAdmin/PanelAdmin.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import "./PanelAdmin.css";
+
+import { events } from "../../config/constant";
+import { Link } from "react-router-dom";
+import CardEvent from "../../components/card-event/CardEvent";
+
 const PanelAdmin = () => {
-  return <div>Panel</div>;
+  return (
+    <div className="panelAdminMainContainer">
+      <p className="panelAdminTitle">Gérer vos événements</p>
+      <button className="panelAdminAddEventButton">Ajouter un événement</button>
+      <div className="panelAdminCardEvent">
+        {events.map((event, index) => (
+          <Link to={`/${event.title}`} key={index}>
+            <CardEvent
+              eventTitle={event.title}
+              eventDate={event.dates}
+              eventImage={event.cover}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PanelAdmin;
