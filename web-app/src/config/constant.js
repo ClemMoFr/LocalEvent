57f4f06179f1d82c7d999df5c7804d9560f5114d
Clément <clmentmoreno@hotmail.com>
2023-09-02 16:40:20 +0200

    CardEventDetails ok

--- web-app/src/config/constant.js
+++ web-app/src/config/constant.js
@@ -8,9 +8,9 @@ export const events = [
     id: 1,
     title: "foire du trone",
     cover: event1,
-    summary:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat. Curabitur eu mattis lacus, ac ",
-    dates: "Du 17 septembre au 23 septembre",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat. Curabitur eu mattis lacus, ac Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat.  ",
+    dates: "Du 17 au 23 septembre",
     lien: "https://www.google.com/",
     adresse: "2 rue des Alouettes, Lyon 69320",
     coordonnées: [45.76139527856017, 4.864856421807162],
@@ -19,9 +19,9 @@ export const events = [
     id: 2,
     title: "Vogue des marrons",
     cover: event2,
-    summary:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat. Curabitur eu mattis lacus, ac ",
-    dates: "Du 02 septembre au 12 septembre",
+    dates: "Du 02 au 12 septembre",
     lien: "https://www.google.com/",
     adresse: "32 avenue Corte, Lyon 69310",
     coordonnées: [45.74739517505285, 4.859755811239093],
@@ -30,9 +30,9 @@ export const events = [
     id: 3,
     title: "Braderie fondations Deguins",
     cover: event3,
-    summary:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat. Curabitur eu mattis lacus, ac ",
-    dates: "Du 12 septembre au 13 septembre",
+    dates: "Du 12 au 13 septembre",
     lien: "https://www.google.com/",
     adresse: "43 Grande Rue de Lyon, Lyon 69220",
     coordonnées: [45.74791053282803, 4.826894065028693],
@@ -41,9 +41,9 @@ export const events = [
     id: 4,
     title: "Les petites patineuses",
     cover: event4,
-    summary:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus elit ac neque molestie, sit amet pulvinar sem dictum. In at laoreet leo. Donec pharetra volutpat imperdiet. Proin semper vitae tortor id consequat. Curabitur eu mattis lacus, ac ",
-    dates: "Du 19 septembre au 26 septembre",
+    dates: "Du 19 au 26 septembre",
     lien: "https://www.google.com/",
     adresse: "12 Grande Rue de Lyon, Lyon 69324",
     coordonnées: [45.75563477120552, 4.832738514637804],
