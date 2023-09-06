d84087ca5cae30c2ab017bb6b9b4e42fc04438ce
Clément <clmentmoreno@hotmail.com>
2023-09-06 09:00:19 +0200

    homepage -> map marker set

--- web-app/src/pages/Home/HomePage.jsx
+++ web-app/src/pages/Home/HomePage.jsx
@@ -8,14 +8,23 @@ import CardEvent from "../../components/card-event/CardEvent";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  // Déclaration de la variable map en dehors des useEffect
+  const [map, setMap] = useState(null);
+
   useEffect(() => {
-    const map = L.map("map").setView([45.75, 4.85], 13);
+    // Créez la carte et définissez-la dans la variable map
+    const newMap = L.map("map").setView([45.75, 4.85], 13);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(newMap);
+
+    // Mettez à jour la variable map avec la nouvelle carte
+    setMap(newMap);
+
     return () => {
-      map.remove();
+      // Supprimez la carte lorsque le composant est démonté
+      newMap.remove();
     };
   }, []);
 
@@ -31,6 +40,18 @@ const HomePage = () => {
     })();
   }, []);
 
+  useEffect(() => {
+    // Ajoutez les marqueurs à la carte en utilisant la variable map
+    if (map && events) {
+      events.forEach((event, index) => {
+        L.marker([event.eventLat, event.eventLon])
+          .addTo(map)
+          .bindPopup(`<b>${event.eventTitle}</b><br>${event.eventAddress}`)
+          .openPopup();
+      });
+    }
+  }, [map, events]);
+
   return (
     <div className="homePageMainContainer">
       <div id="map" className="leaflet-map"></div>
@@ -41,7 +62,7 @@ const HomePage = () => {
             <p>Loading...</p>
           ) : (
             events.map((event, index) => (
-              <Link to={`/${event.eventTitle}`} key={index}>
+              <Link to={`/${event.title}`} key={index}>
                 <CardEvent
                   eventTitle={event.eventTitle}
                   eventDate={event.eventDate}
