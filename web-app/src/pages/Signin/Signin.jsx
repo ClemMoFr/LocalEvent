9bdb280716c9cc32d14312c0cdef97ab859daf3f
Clément <clmentmoreno@hotmail.com>
2023-09-09 15:09:53 +0200

    signin ok

--- web-app/src/pages/Signin/Signin.jsx
+++ web-app/src/pages/Signin/Signin.jsx
@@ -12,6 +12,7 @@ const Signin = () => {
   const [error, setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const userRole = "utilisateur";
 
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
@@ -43,10 +44,44 @@ const Signin = () => {
     setShowConfirmPassword(!showConfirmPassword);
   };
 
+  async function createUser(userName, userEmail, userPassword, userRole) {
+    try {
+      const response = await fetch("http://localhost:4000/user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userName,
+          userEmail,
+          userPassword,
+          userRole,
+        }),
+      });
+      const newUser = await response.json();
+      if (!response.ok) {
+        throw Error(await response.json());
+      } else {
+        // reload();
+      }
+      return newUser;
+    } catch (error) {
+      throw Error("Oups ! Un des champs est manquant ou mal renseigné");
+    }
+  }
+
   return (
     <div className="signInMainContainer">
       <div className="signInLogo">LocalEvent.</div>
-      <form>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          try {
+            await createUser(userName, userMail, password, userRole);
+            alert(
+              `l'utilisateur à été créer avec ${userName},${userMail},${password},${userRole} `
+            );
+          } catch (error) {}
+        }}
+      >
         <label>
           <p>votre prénom</p>
           <input
@@ -99,7 +134,9 @@ const Signin = () => {
             )}
           </div>
         </label>
-        <button className="settingsButton">S'enregistrer</button>
+        <button className="settingsButton" type="submit">
+          S'enregistrer
+        </button>
       </form>
     </div>
   );
